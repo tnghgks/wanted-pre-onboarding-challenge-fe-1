72f8b5321ccf491f3b23bd1726f0adc0b648f3c5
unknown <tnghgks@naver.com>
2023-01-06 14:29:00 +0900

    feat: 로그인 기능 구현

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { axiosInstance } from "../../Api/api";
 
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const handleLogin = useCallback(async (e) => {
+    e.preventDefault();
+    const {
+      email: { value: email },
+      password: { value: password },
+    } = e.target;
+
+    try {
+      const {
+        data: { token, message },
+      } = await axiosInstance.post("/users/login", { email, password });
+
+      alert(message);
+
+      if (token) {
+        localStorage.setItem("token", JSON.stringify(token));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <Container>
+      <h2>로그인 페이지</h2>
+      <Form onSubmit={handleLogin}>
+        <input type="email" name="email" id="email" />
+        <input type="password" name="password" id="password" />
+        <button>로그인</button>
+      </Form>
+      <Link to="/register">회원가입</Link>
+    </Container>
+  );
 }
+
+const Container = styled.section`
+  margin: 0 auto;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
