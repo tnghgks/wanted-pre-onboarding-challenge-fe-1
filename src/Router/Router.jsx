cd4d2b4ad9ceddb1287ad19a70b7f68da05a17c7
unknown <tnghgks@naver.com>
2023-01-06 13:39:11 +0900

    feat: Router 설정 및 페이지 컴포넌트 생성

---
+++ src/Router/Router.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
