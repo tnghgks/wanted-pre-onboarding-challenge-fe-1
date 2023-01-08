76cb8ea5d12eb572b7f5808f6164cc948d19aa31
tnghgks <tnghgks@naver.com>
2023-01-09 02:02:30 +0900

    feat: GrobalStyled 설정 및 reset CSS 설정

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,10 +4,33 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import ToDoDetail from "../Pages/ToDoDetail/ToDoDetail";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyled = createGlobalStyle`
+${reset}
+
+button{
+  border:none;
+  border-radius: 10px;
+  background-color: #008CBA;
+  color:white;
+  padding: 10px 10px;
+  font-weight: 700;
+  cursor: pointer;
+}
+a{
+  color:inherit;
+}
+a:hover{
+  color:#C0DEFF;
+}
+`;
 
 export default function Router() {
   return (
     <BrowserRouter>
+      <GlobalStyled />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
