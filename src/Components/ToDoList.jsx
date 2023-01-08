1adbd328bef3cf56859db9c565beb5fe86734f4f
tnghgks <tnghgks@naver.com>
2023-01-09 01:14:22 +0900

    refactor: props 간소화 및 Delete 버튼 삭제

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export default function ToDoList({ toDoList, handleDeleteToDo, selectToDo }) {
+export default function ToDoList({ toDoList }) {
   return (
     <Container>
       {toDoList.map((toDo) => (
-        <ToDoItem key={toDo.id} onClick={() => selectToDo(toDo)}>
-          <Title>{toDo.title}</Title>
-          <Content>{toDo.content}</Content>
-          <DeleteBtn onClick={() => handleDeleteToDo(toDo.id)}>삭제</DeleteBtn>
-        </ToDoItem>
+        <Link to={`/${toDo.id}`} key={toDo.id}>
+          <ToDoItem>
+            <Title>{toDo.title}</Title>
+            <Content>{toDo.content}</Content>
+          </ToDoItem>
+        </Link>
       ))}
     </Container>
   );
@@ -22,5 +24,3 @@ const ToDoItem = styled.li``;
 const Title = styled.h3``;
 
 const Content = styled.p``;
-
-const DeleteBtn = styled.button``;
