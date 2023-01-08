74cc30e4332602ab05ef41640dbe26d7e4a549ee
tnghgks <tnghgks@naver.com>
2023-01-09 01:12:23 +0900

    feat: Home 컴포넌트 ToDoDetail 중첩 라우팅

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import ToDoDetail from "../Pages/ToDoDetail/ToDoDetail";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/*" element={<Home />}>
+          <Route path=":id" element={<ToDoDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
