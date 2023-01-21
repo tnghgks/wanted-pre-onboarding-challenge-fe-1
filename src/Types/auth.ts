ffdcaf28ab27f64da644e34d27dff971777fd4ac
tnghgks <tnghgks@naver.com>
2023-01-21 21:52:49 +0900

    refactor: types 파일 이름 변경

--- src/Types/auth.ts
+++
@@ -1,4 +0,0 @@
-export interface ILogin {
-  token?: string;
-  message?: string;
-}
---
+++ src/Types/toDo.d.ts
@@ -0,0 +1,23 @@
+export interface IToDo {
+  title: string;
+  content: string;
+  id: string;
+  createAt: string;
+  updatedAt: string;
+}
+
+export interface IAxiosToDo {
+  data: IToDo;
+}
+export interface IToDoList {
+  toDoList: IToDo[];
+}
+export interface IUpdateData {
+  titleValue: string;
+  contentValue: string;
+}
+
+export interface IUpdateToDo {
+  id: string;
+  updateData: {};
+}
