d4e32a3f66a523bf7c5b182e0d8aa16ad4eb3004
tnghgks <tnghgks@naver.com>
2023-01-27 00:47:48 +0900

    refactor: Login 컴포넌트 react-hook-form 적용

--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -1,29 +1,34 @@
 import { AxiosError } from "axios";
-import React, { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useInput } from "../../Hooks/useInput";
-import { validateEmail, validatePassword } from "../../Policy/inputValidate";
-import { login } from "../../Services/auth";
 import { Container, Form, LoginContainer, Title } from "./style";
-import { ILogin } from "../../Types/auth";
+import ErrorMessage from "../Common/ErrorMessage";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { authApi } from "../../Api/Auth/auth";
+import { EMAIL_PATTERN } from "../../Constant/regex";
+
+type FormValues = {
+  email?: string;
+  password?: string;
+};
 
 export default function Login() {
-  const { value: email, onChange: emailOnChange } = useInput("", validateEmail);
-  const { value: password, onChange: passwordOnChange } = useInput("", validatePassword);
+  const [apiError, setApiError] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormValues>();
+
   const navigate = useNavigate();
 
-  const handleLogin = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const target = e.target as typeof e.target & {
-        email: { value: string };
-        password: { value: string };
-      };
-      const email = target.email.value;
-      const password = target.password.value;
-
+  const handleLogin: SubmitHandler<FormValues> = async (data) => {
+    const { email, password } = data;
+    if (typeof email === "string" && typeof password === "string") {
       try {
-        const { token, message } = (await login(email, password)) as ILogin;
+        const {
+          data: { token, message },
+        } = await authApi.login(email, password);
 
         alert(message);
 
@@ -33,12 +38,11 @@ export default function Login() {
         }
       } catch (error) {
         if (error instanceof AxiosError) {
-          console.log(error);
+          setApiError(error.response?.data.details);
         }
       }
-    },
-    [navigate]
-  );
+    }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -51,9 +55,32 @@ export default function Login() {
     <Container>
       <LoginContainer>
         <Title>로그인 페이지</Title>
-        <Form onSubmit={handleLogin}>
-          <input placeholder="이메일을 입력해주세요." value={email} type="email" name="email" id="email" onChange={emailOnChange} required />
-          <input placeholder="비밀번호를 입력해주세요." value={password} type="password" name="password" id="password" minLength={8} onChange={passwordOnChange} required />
+        <Form onSubmit={handleSubmit(handleLogin)}>
+          <input
+            type="email"
+            placeholder="이메일 주소를 입력해주세요."
+            {...register("email", {
+              required: true,
+              pattern: {
+                value: EMAIL_PATTERN,
+                message: "이메일은 @와 .을 포함해야 합니다.",
+              },
+            })}
+          />
+          {errors?.email?.message && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+          <input
+            type="password"
+            placeholder="8자리 이상 패스워드를 입력해주세요."
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 8,
+                message: "8자 이상 입력해주세요.",
+              },
+            })}
+          />
+          {errors?.password?.message && <ErrorMessage>{errors.password.message}</ErrorMessage>}
+          {apiError && <ErrorMessage>{apiError}</ErrorMessage>}
           <button>로그인</button>
         </Form>
         <Link to="/register">회원가입</Link>
