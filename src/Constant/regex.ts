7e6d9d811f09e12b0ac2f683011aaeefa2f4b251
tnghgks <tnghgks@naver.com>
2023-01-26 23:31:04 +0900

    feat: regex 상수 분리

---
+++ src/Constant/regex.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-useless-escape
+export const EMAIL_PATTERN = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
