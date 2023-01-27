8c6c71caf5eb1f3bcfff4d70248e6ee5e1a672fa
tnghgks <tnghgks@naver.com>
2023-01-28 06:18:54 +0900

    refactor: 불필요한 코드 삭제

--- src/Hooks/Queries/ToDo/useGetToDoById.tsx
+++ src/Hooks/Queries/ToDo/useGetToDoById.tsx
@@ -1,21 +1,12 @@
 import { AxiosResponse } from "axios";
 import { useQuery } from "react-query";
 import { toDoApi } from "../../../Api/ToDo/toDo";
-import { IAxiosToDo } from "../../../Types/toDo";
+import { IAxiosToDo, IToDo } from "../../../Types/toDo";
 
 const useGetToDoById = (id: string | undefined) => {
   return useQuery(["todoDetail", id], () => toDoApi.getToDoById(id), {
-    select: ({ data: { data } }: AxiosResponse<IAxiosToDo>) => {
-      if (data && typeof data === "object") {
-        return data;
-      }
-      return {
-        title: "",
-        content: "",
-        id: "",
-        createAt: "",
-        updatedAt: "",
-      };
+    select: ({ data: { data } }: AxiosResponse<IAxiosToDo>): IToDo => {
+      return data;
     },
     retry: false,
     onError: () => {
