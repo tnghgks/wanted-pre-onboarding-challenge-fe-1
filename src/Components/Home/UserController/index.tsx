0442b67a67a22db4abcada79b11bc1d8c4bbc362
tnghgks <tnghgks@naver.com>
2023-01-29 23:31:23 +0900

    refactor: handleLogout 기능 위임

--- src/Components/Home/UserController/index.tsx
+++ src/Components/Home/UserController/index.tsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { Container, LogoutBtn } from "./style";
 
-export default function UserController({ handleLogout }: { handleLogout: () => void }) {
+export default function UserController() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <Container>
       <LogoutBtn onClick={handleLogout}>로그아웃</LogoutBtn>
--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,51 +1,29 @@
 import React, { useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ToDoList from "./ToDoList/index";
 import { Container, CreateBtn, ToDoContainer } from "./style";
-import useCreateToDo from "../../Hooks/Mutation/ToDo/useCreateToDo";
 import ToDoCreator from "./ToDoCreator";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquarePlus } from "@fortawesome/free-solid-svg-icons";
 import UserController from "./UserController";
 import useGetToDoList from "../../Hooks/Queries/ToDo/useGetToDoList";
 
-interface IEventTarget extends EventTarget {
-  todoTitle: HTMLInputElement;
-  todoContent: HTMLInputElement;
-}
-
 export default function Home() {
   const { isLoading, data: toDoList } = useGetToDoList();
   const [isOpenCreator, setIsOpenCreator] = useState(false);
-  const { mutate: createToDoMutate } = useCreateToDo();
-  const navigate = useNavigate();
 
-  const handleCreateToDo = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const target = e.target as IEventTarget;
-    const todoTitle = target.todoTitle;
-    const todoContent = target.todoContent;
-
-    createToDoMutate({ title: todoTitle.value, content: todoContent.value });
-    todoTitle.value = "";
-    todoContent.value = "";
-    setIsOpenCreator(false);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
+  const ToggleOpenCreator = () => {
+    setIsOpenCreator((prev) => !prev);
   };
 
   return (
     <Container>
       <ToDoContainer>
-        <UserController handleLogout={handleLogout} />
+        <UserController />
         <CreateBtn onClick={() => setIsOpenCreator((prev) => !prev)}>
           <FontAwesomeIcon icon={faSquarePlus} color="#61876E" size="2x" />
         </CreateBtn>
-        {isOpenCreator ? <ToDoCreator handleCreateToDo={handleCreateToDo} handleLogout={handleLogout} /> : null}
+        {isOpenCreator ? <ToDoCreator ToggleOpenCreator={ToggleOpenCreator} /> : null}
         {isLoading ? "Loading" : <ToDoList toDoList={toDoList} />}
       </ToDoContainer>
       <Outlet />
