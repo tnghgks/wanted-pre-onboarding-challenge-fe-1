6e082f8b85d13cff4020afa01c94fc12c0a72659
tnghgks <tnghgks@naver.com>
2023-01-30 23:42:40 +0900

    design: login 컴포넌트 스타일 변경

--- src/Components/Login/style.tsx
+++ src/Components/Login/style.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.section`
+export const Container = styled.main`
   margin: 0 auto;
   display: flex;
   flex-direction: column;
@@ -8,18 +8,20 @@ export const Container = styled.section`
   align-items: center;
   width: 100%;
   height: 100vh;
-  background-color: #fff8e1;
 `;
 
 export const LoginContainer = styled.div`
-  background-color: #c0deff;
+  width: 800px;
   border-radius: 20px;
   padding: 30px;
   text-align: center;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 `;
 
 export const Title = styled.h2`
   font-size: 2rem;
+  font-weight: 700;
 `;
 
 export const Form = styled.form`
@@ -28,4 +30,12 @@ export const Form = styled.form`
   justify-content: center;
   margin: 10px 0px;
   gap: 10px;
+  input {
+    font-size: 1.3rem;
+    padding: 5px;
+  }
+  button {
+    font-size: 1.3rem;
+    font-weight: 700;
+  }
 `;
